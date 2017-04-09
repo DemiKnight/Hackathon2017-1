7ae520e5d32d76b833bedc9e2b6cfe7e8e3fc01d
Kryptical <alexknight133@icloud.com>
2017-04-09 12:12:43 +0100

    WIP Game | Buttons + starting backend js

--- src/js/MemoryGame.js
+++ src/js/MemoryGame.js
@@ -6,6 +6,11 @@
 
 // var ctx = canvas.getContext("2d");
 
+const speed = 1;
+let complexity = 5;
+
+let pattern = new Array;
+
 function DrawSquare(canvasElement) {
     var ctx = canvasElement.getContext("2d");
 
@@ -16,4 +21,30 @@ function DrawSquare(canvasElement) {
     ctx.closePath();
 }
 
+function checkPattern() {
 
+}
+
+function generatePattern() {
+
+    for(index = 0; index < complexity ;index++){
+        pattern.add(Math.floor(Math.random() * (7)) + 1)
+        pattern[]
+    }
+
+
+}
+
+function buttonPressed(buttonNum){
+    //alert('The button ' & buttonNum & ' has been pressed!')
+    console.log('The button being pressed is ' + buttonNum.toString())
+    let button = document.getElementById("btn" + buttonNum)
+
+    button.style.opacity = 0.5
+
+}
+// $(function () {
+//     $("button")
+//
+// })
+//
